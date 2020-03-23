bc6b53b73ab2423e0713db6336529cbd34e838a3
Yein Narayana <mrneverflatz@gmail.com>
2020-03-24 03:35:22 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "assets/scss/style.scss";
+
+function App() {
+  return <div className="App">hai dunia!</div>;
+}
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+import moduleName from "elements/Button";
+export default class LandingPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
